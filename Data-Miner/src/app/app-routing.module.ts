2c35e17fd3abe130307abf783c0f7eb1b6baff6b
IvanGonzalez6 <rei0406@hotmail.com>
2021-02-21 02:17:10 -0600

    Vista de Login y 404

---
+++ Data-Miner/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UploadFileComponent } from './upload-file/upload-file.component';
+import { DisplayComponent } from './display/display.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'upload', component: UploadFileComponent },
+  { path: 'display', component: DisplayComponent },
+  { path: 'notFound', component: NotFoundComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- Data-Miner/src/app/app.module.ts
+++ Data-Miner/src/app/app.module.ts
@@ -2,15 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DisplayComponent } from './display/display.component';
+import { RegisterComponent } from './register/register.component';
+import { UploadFileComponent } from './upload-file/upload-file.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DisplayComponent,
+    RegisterComponent,
+    UploadFileComponent,
+    NotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
